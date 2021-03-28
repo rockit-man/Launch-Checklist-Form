d828533cba0877a495a963591f9596db32314485
rockit-man <demetrius.esq@gmail.com>
2021-03-28 14:16:30 -0500

    completed shuttle requirements

--- script.js
+++ script.js
@@ -2,35 +2,73 @@
 
 window.addEventListener("load", () => {
 
-   let form = document.getElementById("launchForm");
+   const form = document.getElementById("launchForm");
    
    form.addEventListener("submit", (event) => {
 
-      let pilotNameInput = document.querySelector("input[name=pilotName]").value;
-      let copilotNameInput = document.querySelector("input[name=copilotName]").value;
-      let fuelLevelInput = document.querySelector("input[name=fuelLevel]").value;
-      let cargoMassInput = document.querySelector("input[name=cargoMass]").value;
-   
-      if (pilotNameInput === "" || copilotNameInput === "" || fuelLevelInput === "" || cargoMassInput === "") {
-         event.preventDefault();
+      event.preventDefault();
+
+      const pilotNameInput = document.querySelector("input[name=pilotName]").value;
+      const copilotNameInput = document.querySelector("input[name=copilotName]").value;
+      const fuelLevelInput = document.querySelector("input[name=fuelLevel]").value;
+      const cargoMassInput = document.querySelector("input[name=cargoMass]").value
+
+      
+      let pilotStatus = document.getElementById("pilotStatus");
+      let copilotStatus = document.getElementById("copilotStatus");
+      let fuelStatus = document.getElementById("fuelStatus");
+      let cargoStatus = document.getElementById("cargoStatus");
+      let launchStatus = document.getElementById("launchStatus");
+
+      document.getElementById("faultyItems").style.visibility = "visible"
+
+      if (pilotNameInput === ""
+         || copilotNameInput === ""
+         || fuelLevelInput === ""
+         || cargoMassInput === "") {
          alert("All fields are required!");
       }
-      if (!isNaN(pilotNameInput)) {
-         event.preventDefault();
-         alert("'Pilot Name' entry is invalid. Please validate 'Pilot Name' is a string of letters.");
+
+      if (isNaN(fuelLevelInput) || isNaN(cargoMassInput)) {
+         alert("Both 'Fuel Level' & 'Cargo Mass' must be a number or string of numbers containing only digits from 0-9.");
+         launchStatus.innerHTML = "Shuttle not ready for launch"
+         launchStatus.style.color = "red"
+      } 
+      
+      if (!isNaN(pilotNameInput) || !isNaN(copilotNameInput)) {
+         alert("'Pilot' or 'Co-pilot' name entry is invalid. Please validate entry is a string of letters.");
+         pilotStatus.innerHTML = "Pilot not ready"
+         copilotStatus.innerHTML = "Co-pilot not ready"
+         launchStatus.innerHTML = "Shuttle not ready for launch"
+         launchStatus.style.color = "red"
       }
-      if (!isNaN(copilotNameInput)) {
-         event.preventDefault();
-         alert("'Co-pilot Name' entry is invalid. Please validate 'Co-pilot Name' is a string of letters.");
+      
+      if (Number(fuelLevelInput) < 10000) {
+         fuelStatus.innerHTML = "There isn't enough fuel for the journey"
+         launchStatus.innerHTML = "Shuttle not ready for launch"
+         launchStatus.style.color = "red"
       }
-      if (isNaN(fuelLevelInput)) {
-         event.preventDefault();
-         alert("'Fuel Level' must be a number or string of numbers containing only digits from 0-9.");
+      
+      if (Number(cargoMassInput) > 10000) {
+         cargoStatus.innerHTML = "Too much mass on board, Shuttle cannot takeoff"
+         launchStatus.innerHTML = "Shuttle not ready for launch"
+         launchStatus.style.color = "red"
+      } 
+      
+      if (isNaN(pilotNameInput)
+         && isNaN(copilotNameInput)
+         && Number(fuelLevelInput) >= 10000
+         && Number(cargoMassInput) <= 10000) {
+         pilotStatus.innerHTML = `${pilotNameInput} Ready`
+         copilotStatus.innerHTML = `${copilotNameInput} Ready`
+         fuelStatus.innerHTML = "Fuel level high enough for launch"
+         cargoStatus.innerHTML = "Cargo mass low enough for launch"
+         launchStatus.innerHTML = "Shuttle is ready for launch"
+         launchStatus.style.color = "green"
       }
-      if (isNaN(cargoMassInput)) {
-         event.preventDefault();
-         alert("'Cargo Mass' must be a number or string of numbers containing only digits from 0-9.");
-      }
+
+
+
    });
 });
 
